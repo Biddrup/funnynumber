e8ae8252f9e03f94f68bef6e6b20b18d8673640d
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-10-25 14:08:33 +0600

    Add files via upload

---
+++ loopnumber.py
@@ -0,0 +1,35 @@
+#I love number and always try to play them
+# Before going through this code, you sould watch this one
+# https://www.youtube.com/watch?v=d8TRcZklX_Q
+
+# I was trying to figure out what about the rest, like 5, 6, 7, 8 or n digits
+#And, I have got something really intersting
+#If you try, you will get amazed 
+
+
+# Will count your given number
+e = []
+
+while True:
+    #Write down a number 9 < n, if you want to continue the loop
+    n = int(input('Please, give me a number: '))
+    
+    if n == 0 <= 9:
+        break
+    else:
+        #Will ascending number
+        a = int(''.join(sorted(str(n))))
+
+        #Will descending number
+        b = int(''.join(sorted(str(n), reverse = True)))
+
+        # Going to subtract max to min
+        c = b - a 
+        e = e + [n]
+        print (e)
+        print ('New number = ' + str(c) + ', write down the new number')
+final = len(e)
+lastvalue = final - (final + 1)
+
+print ('Complete list' + str(e))
+print ('Your final decesion is '+ str(e[lastvalue]))
